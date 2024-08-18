eb3f5e1e7a18946e30bfb7d5f51295f9ace985eb
Danupa0 <161790821+Danupa0@users.noreply.github.com>
2024-08-18 18:31:51 +0530

    💃𝐐𝐔𝐄𝐄𝐍 𝐊𝐄𝐍𝐙𝐈 𝐌𝐃 🤍
    💃𝐐𝐔𝐄𝐄𝐍 𝐊𝐄𝐍𝐙𝐈 𝐌𝐃 🤍

--- config.js
+++ config.js
@@ -1,1 +1,12 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID,|| "sdZhDJjY#bM6l8gzdgvtO6shqyHJyJtCx3jqzH_ILNLKJKfe04ZM"
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/c2e4fe813f18339da904a.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*🙋🏼✨ Hey, User......!*                   ____________________________                                    💃 𝐐𝐔𝐄𝐄𝐍 𝐊𝐄𝐍𝐙𝐈 𝐌𝐃 🤍╭─•••••••••••••••••••••••••◯                              │🛟                 *A MULTI DEVICE         WHATSAPP BOT*           ╰─•••••••••••••••••••••••••◯",
+};
 
