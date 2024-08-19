b609f2c02e1407e1621795c5ca3c40ee4e698875
Danupa0 <161790821+Danupa0@users.noreply.github.com>
2024-08-19 15:33:26 +0530

    💃𝐐𝐔𝐄𝐄𝐍 𝐊𝐄𝐍𝐙𝐈 𝐌𝐃 🤍
    💃𝐐𝐔𝐄𝐄𝐍 𝐊𝐄𝐍𝐙𝐈 𝐌𝐃 🤍

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID|| "sdZhDJjY#bM6l8gzdgvtO6shqyHJyJtCx3jqzH_ILNLKJKfe04ZM",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/c2e4fe813f18339da904a.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "*🙋🏼✨ Hey, User......!*                   ____________________________                                    💃 𝐐𝐔𝐄𝐄𝐍 𝐊𝐄𝐍𝐙𝐈 𝐌𝐃 🤍╭─•••••••••••••••••••••••••◯                              │🛟                 *A MULTI DEVICE         WHATSAPP BOT*           ╰─•••••••••••••••••••••••••◯",
+ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/c2e4fe813f18339da904a.jpg",
+ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲 𝐈'𝐦 💃𝐐𝐔𝐄𝐄𝐍 𝐊𝐄𝐍𝐙𝐈 𝐌𝐃 🤍 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 𝐁𝐨𝐭🪀*\n\n*🔔𝐈'𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰💖*\n\n*⚖️𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 - : © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴀʀᴋɴᴇᴏɴᴄʏʙᴇʀꜱ*",    
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
